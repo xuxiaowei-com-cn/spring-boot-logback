081d6ebda281c15ab1148036ba413df437db8609
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-07-06 16:25:17 +0800

    日志分包

---
+++ src/main/java/cn/com/xuxiaowei/amodule/controller/AmoduleRestController.java
@@ -0,0 +1,27 @@
+package cn.com.xuxiaowei.amodule.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * A 模块
+ *
+ * @author 徐晓伟
+ */
+@Slf4j
+@RestController
+public class AmoduleRestController {
+
+    @RequestMapping("/a")
+    public String a(HttpServletRequest request) {
+        log.debug("A Module - " + MDC.get("serial"));
+        log.info("A Module - " + MDC.get("serial"));
+        log.error("A Module - " + MDC.get("serial"));
+        return request.getSession().getId();
+    }
+
+}
---
+++ src/main/java/cn/com/xuxiaowei/bmodule/controller/BmoduleRestController.java
@@ -0,0 +1,27 @@
+package cn.com.xuxiaowei.bmodule.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * B 模块
+ *
+ * @author 徐晓伟
+ */
+@Slf4j
+@RestController
+public class BmoduleRestController {
+
+    @RequestMapping("/b")
+    public String b(HttpServletRequest request) {
+        log.debug("B Module - " + MDC.get("serial"));
+        log.info("B Module - " + MDC.get("serial"));
+        log.error("B Module - " + MDC.get("serial"));
+        return request.getSession().getId();
+    }
+
+}
